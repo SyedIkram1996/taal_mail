329d3c4be6ed2f9b7a5eb08b9ceb3ecb75485be5
SyedIkram1996 <ikram96211@gmail.com>
2024-10-12 02:17:20 +0500

    Project Initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></Box>
+    </Container>
+  );
+}
