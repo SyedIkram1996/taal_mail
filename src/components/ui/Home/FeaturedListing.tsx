20dfd5fde5c05697620f40de94a853d9684642c5
SyedIkram1996 <ikram96211@gmail.com>
2024-11-02 23:30:50 +0500

    feat: featured listing fallback in suspense

--- src/components/ui/Home/FeaturedListing.tsx
+++ src/components/ui/Home/FeaturedListing.tsx
@@ -1,8 +1,6 @@
-import TextSm from "@/components/common/Text/TextSm";
 import TextXs from "@/components/common/Text/TextXs";
 import { FEATURED_PROPERTIES } from "@/constants/api.routes";
 import { IProperty } from "@/interfaces/IProperty";
-import { Stack } from "@mui/material";
 import FeaturedListingSwiper from "./FeaturedListingSwiper";
 
 const FeaturedListing = async () => {
@@ -11,29 +9,13 @@ const FeaturedListing = async () => {
     await response.json();
 
   return (
-    <Stack
-      sx={{
-        pt: "11.09rem",
-        width: { xs: "100%", md: "66.5625rem" },
-        pb: "4.31rem",
-        px: { xs: "1rem", md: "0" },
-      }}
-    >
-      <TextSm
-        text="Featured Listings"
-        sx={{
-          fontSize: "2rem",
-          color: "var(--text-secondary)",
-          textAlign: "start",
-          pb: "1.91rem",
-        }}
-      />
+    <>
       {data.error ? (
         <TextXs text="No featured listing" />
       ) : (
         <FeaturedListingSwiper featuredListing={data.properties} />
       )}
-    </Stack>
+    </>
   );
 };
 
